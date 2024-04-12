812485511a44870dbab99deafcf96fd7ecf9dbc8
deepak mehra <deepak7mhera7@gmail.com>
2024-04-12 12:16:24 +0530

    final commmit

--- apps/user-app/app/lib/actions/p2pTransfer.tsx
+++ apps/user-app/app/lib/actions/p2pTransfer.tsx
@@ -32,6 +32,7 @@ export default async function p2pTransfer(to:string,amount:number) {
 
     
     await db.$transaction(async (tx)=>{
+        await tx.$queryRaw`SELECT * FROM "Balance" WHERE "userId" = ${Number(from)} FOR UPDATE`;
         const fromBalance = await tx.balance.findUnique({
             where: {userId:Number(from)}
         })
